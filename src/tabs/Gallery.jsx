5cc2a2014afcea1e01b9c686bfa681b6099bd61c
charivna <124678194+charivna@users.noreply.github.com>
2023-09-10 13:55:23 +0200

    push

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,10 +4,20 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    value: '',
+  };
+
+  onSubmit = value => {
+    this.setState({
+      value,
+    });
+  };
   render() {
     return (
       <>
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        <SearchForm onSubmit={this.onSubmit} />
       </>
     );
   }
